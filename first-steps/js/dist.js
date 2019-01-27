89033b0921102c666887b27612910c0bce96286f
MrGregor228 <39625405+MrGregor228@users.noreply.github.com>
2019-01-27 20:42:06 +0300

    commit

---
+++ first-steps/js/dist.js
@@ -0,0 +1,5 @@
+﻿function toggle(el) {
+    el.style.display = (el.style.display == 'block') ? '' : 'block'
+    el.style.position = (el.style.display == 'absolute') ? '' : 'absolute'
+    el.style.zIndex = (el.style.display == '9999') ? '' : '9999'
+}
---
+++ js/smooth_scrolling.js
@@ -0,0 +1,23 @@
+﻿$(document).ready(function(){
+    $(".navig").on("click","a", function (event) {
+
+        event.preventDefault();
+ 
+        var id  = $(this).attr('href'),
+ 
+            top = $(id).offset().top;
+         
+        $('body,html').animate({scrollTop: top}, 1000);
+    });
+
+    $(".top_button").on("click","a", function (event) {
+
+        event.preventDefault();
+ 
+        var id  = $(this).attr('href'),
+ 
+            top = $(id).offset().top;
+         
+        $('body,html').animate({scrollTop: top}, 1000);
+    });
+});
