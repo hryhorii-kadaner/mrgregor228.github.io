80975b6da199a98326facbc5ac86245a9cfa66bd
MrGregor228 <39625405+MrGregor228@users.noreply.github.com>
2019-02-28 19:05:22 +0300

    init commit

--- js/smooth_scrolling.js
+++ js/smooth_scrolling.js
@@ -20,4 +20,11 @@
          
         $('body,html').animate({scrollTop: top}, 1000);
     });
+
+    $(".sec1").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
 });
