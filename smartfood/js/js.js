f0bd30867130d97777182c7398d26cc2318f0898
MrGregor228 <39625405+MrGregor228@users.noreply.github.com>
2019-04-12 18:05:22 +0300

    commit

---
+++ smartfood/js/js.js
@@ -0,0 +1,11 @@
+document.getElementById("dd").onclick = function () {
+    window.open('http://dd.coral-club.com/ru/4059487.html');
+};
+
+document.getElementById("slim").onclick = function () {
+    window.open('https://ua.coral-club.com/shop/products/91674.html?REF_CODE=140947148014');
+};
+
+document.getElementById("barrrr").onclick = function () {
+    window.open('https://ua.coral-club.com/shop/products/91684.html?REF_CODE=140947148014');
+};
