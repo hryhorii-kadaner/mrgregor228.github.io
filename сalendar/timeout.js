465a144bcd216881e07fb36f0bfe4cd05863a36e
MrGregor228 <39625405+MrGregor228@users.noreply.github.com>
2019-07-26 10:31:37 +0300

    c

--- сalendar/timeout.js
+++ сalendar/timeout.js
@@ -5,11 +5,13 @@ for (i = 0; i < btns.length; i++) {
   console.log(new Date().getDate() +"/"+parseInt(btn.textContent))
   console.log()
   console.log(parseInt(btn.textContent,10)<new Date().getDate())
-  if (parseInt(btn.textContent,10)<new Date().getDate()){
+  if (parseInt(btn.textContent,10) < new Date().getDate()){
+    btn.title = 'Выберите другой день!'; 
     btn.classList.add("expired");
-    btn.title = 'Выберите другой день!';
   } else if (parseInt(btn.textContent,10) == new Date().getDate()){
     btn.classList.add("active");
+    btn.title = 'День свободен!';
+  } else if (parseInt(btn.textContent,10) >= new Date().getDate()){
     btn.title = 'День свободен!';
   }
 }
